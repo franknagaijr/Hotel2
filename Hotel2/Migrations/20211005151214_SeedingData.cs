910a96876ab8d30a680bbfe5a09936b87c9fe473
Dan.Wolcott <dan.wolcott@infor.com>
2021-10-04 16:31:01 -0400

    Add project files.

---
+++ Hotel2/Migrations/20211005151214_SeedingData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel2.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "United States", "USA" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "Spain", "ESP" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "Switzerland", "CH" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 1, "Cherry Moon", 1, "USA No 1", 1.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 2, "Cherry Luna", 2, "ESP No 1", 1.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 3, "Cherry Mond", 3, "CH No 1", 1.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ Hotel2/data/DatabaseContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel2.data
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options): base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                { 
+                    Id=1, 
+                    Name="United States", 
+                    ShortName = "USA"
+                },
+                 new Country
+                 {
+                     Id = 2,
+                     Name = "Spain",
+                     ShortName = "ESP"
+                 },
+                  new Country
+                  {
+                      Id = 3,
+                      Name = "Switzerland",
+                      ShortName = "CH"
+                  }
+                );
+            builder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   Id = 1,
+                   Name= "USA No 1",
+                   CountryId=1,
+                   Address = "Cherry Moon",
+                   Rating = 1
+               },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "ESP No 1",
+                    CountryId = 2,
+                    Address = "Cherry Luna",
+                    Rating = 1
+                },
+                 new Hotel
+                 {
+                     Id = 3,
+                     Name = "CH No 1",
+                     CountryId = 3,
+                     Address = "Cherry Mond",
+                     Rating = 1
+                 }
+               );
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
