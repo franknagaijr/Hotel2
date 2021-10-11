56de3a133824238eef24770b76be546ae3f69b17
Dan.Wolcott <dan.wolcott@infor.com>
2021-10-11 16:22:40 -0400

    chapter 5 section 3

--- Hotel2/Controllers/CountryController.cs
+++ Hotel2/Controllers/CountryController.cs
@@ -43,5 +43,22 @@ namespace Hotel2.Controllers
                 return StatusCode(500, "Internal Server Error. Please try back later.");
             }
         }
-   }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(q => q.Id== id, new List<string> {"Hotels" });
+                var result = _mapper.Map<CountryDTO>(country);
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something is amiss: {nameof(GetCountry)}");
+                return StatusCode(500, "Internal Server Error. Please try back later.");
+            }
+        }
+    }
 }
